d885cd5546524a436a3bbca35133192a20072ff1
jsz <935402935@qq.com>
2018-08-14 15:36:51 +0800

    sub

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var booksRouter = require('./routes/books');
 
-var app = express(asdsads);
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
